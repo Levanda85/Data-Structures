9b9028edd96c63d38cb4036f3fa0c094a30c4f5b
Levanda85 <67434440+Levanda85@users.noreply.github.com>
2022-03-10 20:06:43 +0300

    BST is done

--- src/binarysearchtree/BinarySearchTree.kt
+++ src/binarysearchtree/BinarySearchTree.kt
@@ -24,18 +24,18 @@ class BinarySearchTree {
         if (prev!!.data > data) prev.left = node else prev.right = node
     }
 
-    fun contains(value: Int): Boolean {
+    fun contains(data: Int): Boolean {
         var currentNode = root
-        while (currentNode?.data != value) {
+        while (currentNode?.data != data) {
             when {
-                value < currentNode!!.data -> {
+                data < currentNode!!.data -> {
                     if (currentNode.left != null) {
                         currentNode = currentNode.left
                     } else {
                         return false
                     }
                 }
-                value > currentNode.data -> {
+                data > currentNode.data -> {
                     if (currentNode.right != null) {
                         currentNode = currentNode.right
                     } else {
@@ -44,51 +44,76 @@ class BinarySearchTree {
                 }
             }
         }
-        return currentNode.data == value
+        return currentNode.data == data
     }
 
-    /*fun remove(value: Int, parent: BinarySearchTree): Boolean {
-        if (contains(value)) when {
-            value < data && left != null -> {
-                return left!!.remove(value, this)
-            }
-            value < data -> return false
-            value > data && right != null -> return right!!.remove(value, this)
-            value > data -> {
-                return false
-            }
-            else -> {
-                when {
-                    left == null && right == null && this == parent.left -> {
-                        parent.left = null
-                    }
-                    left == null && right == null && this == parent.right -> {
-                        parent.right = null
-                    }
-                    left != null && right == null && this == parent.left -> {
-                        parent.left = this.left
-                    }
-                    left != null && right == null && this == parent.right -> {
-                        parent.right = this.left
-                    }
-                    right != null && left == null && this == parent.left -> {
-                        parent.left = this.right
-                    }
-                    right != null && left == null && this == parent.right -> {
-                        parent.right = this.right
-                    }
-                    else -> {
-                        data = right!!.min()
-                        right!!.remove(data, this)
-                    }
+    fun remove(data: Int): Boolean {
+        if (contains(data)) {
+            var prev: BinarySearchNode? = null
+            var currentNode: BinarySearchNode? = root
+            while (currentNode != null && currentNode.data != data) {
+                if (currentNode.data > data) {
+                    prev = currentNode
+                    currentNode = currentNode.left
+                } else if (currentNode.data <= data) {
+                    prev = currentNode
+                    currentNode = currentNode.right
                 }
-                return true
             }
+            when {
+                currentNode!!.left == null && currentNode.right == null && currentNode == prev!!.left -> {
+                    prev.left = null
+                }
+                currentNode.left == null && currentNode.right == null && currentNode == prev!!.right -> {
+                    prev.right = null
+                }
+                currentNode.left != null && currentNode.right == null && currentNode == prev!!.left -> {
+                    prev.left = currentNode.left
+                }
+                currentNode.left != null && currentNode.right == null && currentNode == prev!!.right -> {
+                    prev.right = currentNode.left
+                }
+                currentNode.right != null && currentNode.left == null && currentNode == prev!!.left -> {
+                    prev.left = currentNode.right
+                }
+                currentNode.right != null && currentNode.left == null && currentNode == prev!!.right -> {
+                    prev.right = currentNode.right
+                }
+                currentNode.data == root!!.data -> {
+                    root!!.right!!.left = root!!.left
+                    root = root!!.right
+                }
+            }
+            return true
         } else {
-            println("$value is not in the tree")
+            println("$data is not in the tree")
             return false
         }
     }
 
-    fun min(): Int = if (left == null) data else left!!.min()*/
+    /* when {
+         left == null && right == null && this == parent.left -> {
+             parent.left = null
+         }
+         left == null && right == null && this == parent.right -> {
+             parent.right = null
+         }
+         left != null && right == null && this == parent.left -> {
+             parent.left = this.left
+         }
+         left != null && right == null && this == parent.right -> {
+             parent.right = this.left
+         }
+         right != null && left == null && this == parent.left -> {
+             parent.left = this.right
+         }
+         right != null && left == null && this == parent.right -> {
+             parent.right = this.right
+         }
+         else -> {
+             data = right!!.min()
+             right!!.remove(data, this)
+         }
+     }
+     return true*/
 }
--- src/binarysearchtree/BinarySearchTreeExperiment.kt
+++ src/binarysearchtree/BinarySearchTreeExperiment.kt
@@ -4,8 +4,10 @@ fun main() {
     val searchTree = BinarySearchTree()
     searchTree.insert(5)
     searchTree.insert(9)
+    searchTree.insert(7)
     searchTree.insert(4)
     searchTree.insert(12)
     searchTree.insert(15)
+    searchTree.remove(5)
     println(searchTree.root)
 }
